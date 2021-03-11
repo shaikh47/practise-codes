bb6c795c3d845d77440175f7ae8d4c1bd805dcf3
omar shaikh <syed15-8699@diu.edu.bd>
2021-03-11 13:32:54 +0600

    fixed a bug for argument passing

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void bubbleSort(int* arr,int length){
+void bubbleSort(int arr[],int length){
     for(int i=0;i<length;i++){
         for(int x=0;x<length-i-1;x++){
             if(arr[x]>arr[x+1]){
@@ -16,7 +16,7 @@ void bubbleSort(int* arr,int length){
 int main(){
     int array[]={9,8,7,6,5,4,3,2,1};
     int length=sizeof(array)/sizeof(array[0]);
-    bubbleSort(&array,length);
+    bubbleSort(array,length);
 
     for(int i=0;i<sizeof(array)/sizeof(array[0]);i++){
         printf("%d ",array[i]);
